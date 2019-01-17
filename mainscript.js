4da74603c6370eef435b256450f5304340a6d3cb
santhosh268842 <42665781+santhosh268842@users.noreply.github.com>
2019-01-17 16:26:04 +0530

    Add files via upload

---
+++ mainscript.js
@@ -0,0 +1,4 @@
+<script> var event = "welcome to techinical event"; function func1() { return
+(event); } console.log(func1()); </script> <script> var asthra =
+"rot13:nfg{wnin_sha}"; function func() { return (asthra); } console.log(func());
+</script>
